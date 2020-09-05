8ac302f0dbee8202a4e08b88704e7f49fee368ff
seungwon2 <dandelionsw2@ewhain.net>
2020-09-05 11:13:33 +0900

    문제선정 완료

---
+++ codeup_python/1023.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+import math
+
+a = float(input("실수를 하나 입력하세요: "))
+int_a = int(a)
+decimal_a = (a - int(a))
+total_length = len(str(decimal_a))-2
+
+print(int_a)
+print(int(decimal_a*int(10**total_length)))
+
+
---
+++ codeup_python/1031.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*- 
+
+decimal = input("10진수 입력: ")
+octal = oct(decimal)
+print(octal)
