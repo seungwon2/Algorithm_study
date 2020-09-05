e421561b30b6f81971335a5137d90c94d4e8394d
seungwon2 <dandelionsw2@ewhain.net>
2020-09-05 15:48:43 +0900

    add 1084

---
+++ codeup_python/1084.py
@@ -0,0 +1,11 @@
+colors = input("빨강 초록 파랑 순서대로 입력: ").split(" ")
+r = int(colors[0])
+g = int(colors[1])
+b = int(colors[2])
+count = 0
+for i in range(0, r):
+    for j in range(0,g):
+        for k in range(0,b):
+            print(i,j,k)
+            count = count +1
+print(count)
