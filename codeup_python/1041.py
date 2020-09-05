55ec7c8959acccc658a4c8c0bae571439f419791
seungwon2 <dandelionsw2@ewhain.net>
2020-09-05 14:58:29 +0900

    add 1041

---
+++ codeup_python/1041.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+first_letter = input("input something: ")
+
+if (type(first_letter)==int):
+    print(first_letter+1)
+elif (type(first_letter)==str):
+    next_letter=chr(ord(first_letter)+1)
+    print(next_letter)
