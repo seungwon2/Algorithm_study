f4943bf4e24666a19a1ccd4b48aa151e2865cc4e
seungwon2 <dandelionsw2@ewhain.net>
2020-09-16 13:18:47 +0900

    :boom: add BOJ 2504

---
+++ baekjoon/recursion/2504.py
@@ -0,0 +1,53 @@
+brackets = input("")
+calculated = []
+
+for bracket in brackets:
+    if bracket == ")":
+        temp = 0
+        while calculated:
+            top = calculated.pop()
+            if top == "(":
+                if temp == 0:
+                    calculated.append(2)
+                else:
+                    calculated.append(2*temp)
+                break
+            elif top == "[":
+                print("0")
+                exit(0)
+            else:
+                if temp == 0:
+                    temp = int(top)
+                else:
+                    temp = temp + int(top)
+    elif bracket == "]":
+        temp = 0
+        while calculated:
+            top = calculated.pop()
+            if top == "[":
+                if temp == 0:
+                    calculated.append(3)
+                else:
+                    calculated.append(3*temp)
+                break
+            elif top == "(":
+                print("0")
+                exit(0)
+            else:
+                if temp == 0:
+                    temp = int(top)
+                else:
+                    temp = temp + int(top)
+    else:
+        calculated.append(bracket)
+
+result = 0
+
+for element in calculated:
+    if type(element) != int:
+        print("0")
+        exit(0)
+    else:
+        result += element
+
+print(result)
