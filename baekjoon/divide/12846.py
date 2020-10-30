e33d4bc8fb1ccf1cca62cc5b5a67ed93d4734ae0
seungwon2 <dandelionsw2@ewhain.net>
2020-10-30 15:49:01 +0900

    :boom: add BOJ 12846 code

--- baekjoon/divide/12846.py
+++ baekjoon/divide/12846.py
@@ -1,14 +1,17 @@
-N = int(input())
+n = int(input())
 pay = list(map(int, input().split()))
-max_pay = []
 
-for i in range(N):
-    for k in range(i, N):
-        if pay[i] > pay[k]:
-            pin = k-1
-            break
-        else:
-            pin = N-1
-    max_pay.append(pay[i]*(pin-i+1))
+result = 0
+cursor = 0
+a = 0
 
-print(max(max_pay))
+pay.append(0)
+stack = [(0, pay[0])]
+for i in range(1, n+1):
+    cursor = i
+    while stack and stack[-1][1] > pay[i]:
+        cursor, temp = stack.pop()
+        result = max(result, temp*(i-cursor))
+    stack.append((cursor, pay[i]))
+
+print(result)
