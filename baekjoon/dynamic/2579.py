8c07323269a866d1666638e5e0bd512d70e9529b
seungwon2 <dandelionsw2@ewhain.net>
2020-11-04 15:07:57 +0900

    add BOJ 2579 code

---
+++ baekjoon/dynamic/2579.py
@@ -0,0 +1,21 @@
+inputNum = int(input())
+stairScores = []
+for i in range(inputNum):
+    inputScore = int(input())
+    stairScores.append(inputScore)
+maxScore = [0] * inputNum
+for i in range(inputNum):
+
+    if i == 0:
+        maxScore[0] = stairScores[i]
+        continue
+    elif i == 1:
+        maxScore[1] = stairScores[0] + stairScores[1]
+        continue
+    elif i == 2:
+        maxScore[2] = max(stairScores[0], stairScores[1]) + stairScores[2]
+        continue
+    maxScore[i] = max(maxScore[i-3] + stairScores[i-1],
+                      maxScore[i-2]) + stairScores[i]
+
+print(maxScore[-1])
