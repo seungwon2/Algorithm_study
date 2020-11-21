b4eb5ae2e0baecdd271f237ff2914a120156bac6
seungwon2 <dandelionsw2@ewhain.net>
2020-11-21 14:25:47 +0900

    :recycle: refactor: add dynamic programing - memo

--- baekjoon/dynamic/9095.py
+++ baekjoon/dynamic/9095.py
@@ -1,12 +1,27 @@
-test_case = int(input())
+from sys import stdin
 
-input_list = []
-for i in range(test_case):
-    input_list.append(int(input()))
-dp = [1, 2, 4]
 
-for i in range(3, max(input_list)):
-    dp.append(dp[i-1]+dp[i-2]+dp[i-3])
+def solution(n):
+    if memo[n] == -1:
+        if n == 1:
+            memo[n] = 1
+            return 1
+        if n == 2:
+            memo[n] = 2
+            return 2
+        if n == 3:
+            memo[n] = 4
+            return 4
+        else:
+            memo[n] = solution(n-1) + solution(n-2) + solution(n-3)
+            return solution(n-1) + solution(n-2) + solution(n-3)
+    else:
+        return memo[n]
 
-for i in input_list:
-    print(dp[i-1])
+
+case = int(input())
+
+for _ in range(case):
+    num = int(stdin.readline())
+    memo = [-1]*(num+1)
+    print(solution(num))
