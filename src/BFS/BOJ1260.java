011cd2673744af4009c3f676e0b7c07abf19ef68
seungwon2 <dandelionsw2@ewhain.net>
2020-12-23 17:11:22 +0900

    :recycle: refactor variable name in BOJ 1260 code

--- src/BFS/BOJ1260.java
+++ src/BFS/BOJ1260.java
@@ -5,43 +5,41 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class BOJ1260 {
-    static int[][] check; //간선 연결상태
-    static boolean[] checked; //확인 여부
-    static int n; //정점개수
-    static int m; //간선개수
-    static int start; //시작정점
+    static int[][] isConnected;
+    static boolean[] visited;
+    static int n;
+    static int m;
+    static int start;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
         start = sc.nextInt();
 
-        check = new int[1001][1001]; //좌표를 그대로 받아들이기 위해 +1해서 선언
-        checked = new boolean[1001]; //초기값 False
+        isConnected = new int[1001][1001];
+        visited = new boolean[1001];
 
-        //간선 연결상태 저장
         for(int i = 0; i < m; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            check[x][y] = check[y][x] = 1;
+            isConnected[x][y] = isConnected[y][x] = 1;
         }
 
-        dfs(start); //dfs호출
+        dfs(start);
 
-        checked = new boolean[1001]; //확인상태 초기화
-        System.out.println(); //줄바꿈
+        visited = new boolean[1001];
+        System.out.println();
 
-        bfs(); //bfs호출
+        bfs();
     }
 
-    //시작점을 변수로 받아 확인, 출력 후 다음 연결점을 찾아 시작점을 변경하여 재호출
     public static void dfs(int i) {
-        checked[i] = true;
+        visited[i] = true;
         System.out.print(i + " ");
 
         for(int j = 1; j <= n; j++) {
-            if(check[i][j] == 1 && !checked[j]) {
+            if(isConnected[i][j] == 1 && !visited[j]) {
                 dfs(j);
             }
         }
@@ -49,18 +47,17 @@ public class BOJ1260 {
 
     public static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start); //시작점도 Queue에 넣어야 함
-        checked[start] = true;
+        queue.offer(start);
+        visited[start] = true;
         System.out.print(start + " ");
 
-        //Queue가 빌 때까지 반복. 방문 정점은 확인, 출력 후 Queue에 넣어 순서대로 확인
         while(!queue.isEmpty()) {
             int temp = queue.poll();
 
             for(int j = 1; j <= n; j++) {
-                if(check[temp][j] == 1 && !checked[j]) {
+                if(isConnected[temp][j] == 1 && !visited[j]) {
                     queue.offer(j);
-                    checked[j] = true;
+                    visited[j] = true;
                     System.out.print(j + " ");
                 }
             }
