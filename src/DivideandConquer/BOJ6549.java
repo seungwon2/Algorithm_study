1adc62ea0db6cbd363f63ecaa20d121459193858
seungwon2 <dandelionsw2@ewhain.net>
2021-02-05 09:14:34 +0900

    :sparkles: add BOJ 6549 code

--- src/DivideandConquer/BOJ6549.java
+++ src/DivideandConquer/BOJ6549.java
@@ -10,6 +10,9 @@ public class BOJ6549 {
     static int num;
     static int right;
     static int left;
+    static int mid;
+    static int hi;
+    static int lo;
     static int result = 0;
     static int[] height;
     public static void main(String[] args) {
@@ -22,19 +25,44 @@ public class BOJ6549 {
             for (int i = 0; i < num; i++) {
                 height[i] = scanner.nextInt();
             }
-            System.out.println(divide(height));
+            System.out.println(divide(0,height.length-1));
         }
 
     }
-    static int divide(int[] height){
-        int length = height.length;
-        for(left=0;left<length;left++){
-            int minHeight = height[left];
-            for(right=left; right<length; right++){
-                minHeight = min(minHeight, height[right]);
-                result = max(result, (right - left + 1) * minHeight);
-            }
+    static int divide(int left, int right){
+        //기저 사례: 판자 1개일때
+        if(left==right){
+            return height[left];
         }
+
+        //오른쪽이나 왼쪽 사각형 각개격파
+        mid = (right + left) / 2;
+        result = max(divide(left, mid), divide(mid+1, right));
+
+        //걸쳐진 사각형 계산
+        //lo, hi 설정
+        lo = mid;
+        hi = mid + 1;
+        int minHeight = min(height[lo], height[hi]);
+        //mid와 mid+1만 포함하는 사각형 넓이 고려!
+        result = max(result, minHeight * 2);
+
+        //left와 right 범위 안에 있을 때 모든 경우 스캔
+        while (lo > left || hi < right) {
+
+            //right 쪽으로 확장할 경우
+            if (hi < right && (lo == left || height[lo - 1] < height[hi + 1])) {
+                hi++;
+                minHeight = min(minHeight, height[hi]);
+            }
+            else {
+                lo--;
+                minHeight = min(minHeight, height[lo]);
+            }
+            //확장 후 사각형 넓이
+            result = max(result, minHeight * (hi - lo + 1));
+        }
+
         return result;
     }
 
