bf86686a72380b103c5a79eeda4d99d5e35c9ca5
seungwon2 <dandelionsw2@ewhain.net>
2021-02-06 22:03:27 +0900

    :sparkles: add BOJ 5904 code

--- src/DivideandConquer/BOJ5904.java
+++ src/DivideandConquer/BOJ5904.java
@@ -1,4 +1,43 @@
 package DivideandConquer;
 
+import java.util.Scanner;
+
 public class BOJ5904 {
+    static int num;
+    static char answer;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        moo(num);
+        System.out.println(answer);
+    }
+
+    static void moo(int num) {
+        int size = 3;
+        int index = 0;
+        if (num == 1) {
+            answer = 'm';
+        }
+        else if (num <= 3) {
+            answer = 'o';
+        }
+        else {
+            while (size < num) {
+                size = size * 2 + index + 4;
+                index++;
+            }
+            int front_back = (size - index - 3) / 2;
+            if (size - front_back + 1 <= num) {
+                moo(num - size + front_back);
+            }
+            else if (num == front_back + 1){
+                answer = 'm';
+            }
+            else{ answer = 'o';
+            }
+        }
+
+    }
+
 }
