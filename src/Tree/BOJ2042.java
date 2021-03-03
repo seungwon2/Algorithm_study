b631ba3ae2badbec5b153911a58f41abf539388c
seungwon2 <dandelionsw2@ewhain.net>
2021-03-03 13:02:59 +0900

    :sparkles: add BOJ 2042 code

--- src/Tree/BOJ2042.java
+++ src/Tree/BOJ2042.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ2042 {
     static long[] arr, tree;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
