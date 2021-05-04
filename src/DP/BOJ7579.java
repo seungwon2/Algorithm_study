ec048be7e11eea4373944175cca34b9b15679221
seungwon2 <dandelionsw2@ewhain.net>
2021-05-04 10:07:29 +0900

    :recycle: refactor : apply dp on BOJ 7579 code

--- src/DP/BOJ7579.java
+++ src/DP/BOJ7579.java
@@ -2,38 +2,39 @@ package DP;
 
 import java.util.Scanner;
 
+import static java.lang.Math.max;
+
 public class BOJ7579 {
     public static void main(String[] args) {
+        //값 입력 받기
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int M = s.nextInt();
-        int memory[] = new int[N+1];
-        int cost[] = new int[N+1];
-        for (int i = 1; i < N+1; i++) {
+        int sum = 0;
+        int memory[] = new int[N];
+        int cost[] = new int[N];
+
+        for (int i = 0; i < N; i++) {
             memory[i] = s.nextInt();
         }
-        for (int i = 1; i < N+1; i++) {
+        for (int i = 0; i < N; i++) {
             cost[i] = s.nextInt();
+            sum += cost[i];
         }
-        int result = 0;
-        while(true){
-            if (M <= 0) {
+        int dp[] = new int[sum + 1];
+
+        //memo[index], index는 cost, value는 확보한 메모리
+        for (int i = 0; i < N; i++) {
+            for (int j = sum; j >= cost[i]; j--) {
+                dp[j] = max(dp[j], dp[j - cost[i]] + memory[i]);
+            }
+        }
+        for (int i = 0; i <= sum; i++) {
+            if (dp[i] >= M) {
+                System.out.println(i);
                 break;
             }
-
-            int tag = 1;
-            int min = cost[1];
-            for (int i = 1; i < N+1; i++) {
-                if (cost[i] <= min) {
-                    min = cost[i];
-                    tag = i;
-                }
-            }
-            M = M - memory[tag];
-            result = result + cost[tag];
-            cost[tag] = Integer.MAX_VALUE;
-
         }
-        System.out.println(result);
     }
+
 }
