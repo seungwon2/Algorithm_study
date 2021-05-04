32424b73207b58f6fe71c377be3ea3d690ea60a7
seungwon2 <dandelionsw2@ewhain.net>
2021-05-04 11:47:20 +0900

    :sparkles: feat : add BOJ 1806 code

--- src/TwoPointer/BOJ1806.java
+++ src/TwoPointer/BOJ1806.java
@@ -1,4 +1,35 @@
 package TwoPointer;
 
+import java.util.Scanner;
+
 public class BOJ1806 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int start = 0;
+        int end = 0;
+        int N = s.nextInt();
+        int S = s.nextInt();
+
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = s.nextInt();
+        }
+        int length = Integer.MAX_VALUE;
+        while (start < N && end < N) {
+            int sum = 0;
+            for (int i = start; i <= end; i++) {
+                sum += numbers[i];
+            }
+            if (sum < S) {
+                end++;
+            } else {
+                length = Math.min(length, end - start + 1);
+                start++;
+                if (end < start) {
+                    end++;
+                }
+            }
+        }
+        System.out.println(length);
+    }
 }
