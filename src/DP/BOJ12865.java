f8862837e35e2344dcf8878c44a20ef253b27e5a
seungwon2 <dandelionsw2@ewhain.net>
2021-05-04 11:09:00 +0900

    :recycle: refactor : apply dp on BOJ 12865 code

--- src/DP/BOJ12865.java
+++ src/DP/BOJ12865.java
@@ -1,49 +1,39 @@
 package DP;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class BOJ12865 {
-    static Integer[][] dp;
-    static int[] W; // weight
-    static int[] V; // value
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        W = new int[N];
-        V = new int[N];
-        dp = new Integer[N][K + 1];
+    static int[] dp;
+    static int[] weight;
+    static int[] value;
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int K = s.nextInt();
+        weight = new int[N];
+        value = new int[N];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+            weight[i] = s.nextInt();
+            value[i] = s.nextInt();
         }
-        System.out.println(knapsack(N - 1, K));
-    }
-    //점화식
-    //if i=0, k=0 --> f(i,k) = 0
-    //if w[i]>k --> f(i,k) = f(i-1,k)
-    //if 0<i , w[i]<=k --> f(i,k) = max( f(i-1, k-w[i]) + V[i],   f(i-1,k))
-    static int knapsack(int i, int k) {
-    // i가 0미만, 즉 범위 밖이 된다면
-        if (i < 0)
-            return 0;
-    // 탐색하지 않은 위치라면?
-        if (dp[i][k] == null) {
-    // 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색)
-            if(W[i] > k) {
-                dp[i][k] = knapsack(i - 1, k);
-            }
-    // 현재 물건(i)을 담을 수 있는 경우
-            else {
-    // 이전 i값과 이전 i값에 대한 k-W[i]의 값 + 현재 가치(V[i])중 큰 값을 저장
-                dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += weight[i];
+        }
+        dp = new int[sum + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = sum; j >= weight[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
             }
         }
-        return dp[i][k];
+        int max = 0;
+        for (int i = 0; i <= K; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
     }
 }
