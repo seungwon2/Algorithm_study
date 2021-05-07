3dc7a480643cb4493c4b5432f3b71ad98270a7a1
seungwon2 <dandelionsw2@ewhain.net>
2021-05-07 19:43:34 +0900

    :sparkles: feat : add BOJ 1715 code

--- src/Greedy/BOJ1715.java
+++ src/Greedy/BOJ1715.java
@@ -1,26 +1,26 @@
 package Greedy;
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class BOJ1715 {
     public static void main(String[] args) {
+        //작은 숫자 뽑기 위해서 Integer형 priority queue 사용
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         //초기화 및 입력 받아 배열 채우기
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
-        int[] numbers = new int[N];
         for (int i = 0; i < N; i++) {
-            numbers[i] = s.nextInt();
+            pq.add(s.nextInt());
         }
 
-        //numbers 작은 순서로 정렬
-        Arrays.sort(numbers);
-
-        int temp = numbers[0];
+        //큐에서 가장 작은 숫자부터 뽑아서 계산
+        int temp_sum = 0;
         int result = 0;
-        for (int i = 1; i < N; i++) {
-            temp += numbers[i];
-            result = result + temp;
+        while (pq.size() > 1) {
+            temp_sum = pq.poll() + pq.poll();
+            result = result + temp_sum;
+            pq.add(temp_sum);
         }
         System.out.println(result);
     }
