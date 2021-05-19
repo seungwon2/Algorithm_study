5333c1718a5699b82170bea2ee6d01891ed4ee81
seungwon2 <dandelionsw2@ewhain.net>
2021-05-19 15:11:46 +0900

    :recycle: refactor : change BOJ1987 code

--- src/DFS/BOJ1987.java
+++ src/DFS/BOJ1987.java
@@ -6,52 +6,59 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1987 {
-    static int R, C;
-    static int[][] map;
-    static boolean[] visit = new boolean[26];
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
-    static int ans = 0;
+    public static int ROW;
+    public static int COL;
+    public static int[][] arr;
 
-    public static void dfs(int x, int y, int count) {
-        if (visit[map[x][y]]) { // 0,0에 저장된 알파벳이 이미 한번 방문한 알파벳이라면,
-            ans = Math.max(ans, count); // 정답을 갱신해준다.
-            return; // 조건에 만족하므로 리턴.
-        } else {
-            visit[map[x][y]] = true;
-            for (int i = 0; i < 4; i++) {
-                int cx = x + dx[i];
-                int cy = y + dy[i];
+    public static int[] dy = {-1, 1, 0, 0}; // 상하 좌우;
+    public static int[] dx = {0, 0, -1, 1};
 
-                if (cx >= 0 && cy >= 0 && cx < R && cy < C) {
-                    dfs(cx, cy, count + 1);
-                }
+    public static boolean[] visited;
 
-            }
-
-            visit[map[x][y]] = false;
-
-        }
-    }
+    public static int step = 1;
+    public static int max_step = 1;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        R = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        map = new int[R][C];
-        for (int i = 0; i < R; i++) {
+        ROW = Integer.parseInt(st.nextToken());
+        COL = Integer.parseInt(st.nextToken());
+        arr = new int[ROW][COL];
+
+        for(int i=0; i < ROW; i++) {
             String str = br.readLine();
-            for (int j = 0; j < C; j++) {
-                map[i][j] = str.charAt(j) - 'A';
+            for(int j=0; j < COL; j++) {
+                arr[i][j] = (int)str.charAt(j) - 65;
             }
         }
 
-        dfs(0, 0, 0);
-        // (0,0)부터 시작하며, 현재 이동한 위치는 0회
+        //알파벳이 26개니까
+        visited = new boolean[26];
+        dfs(0, 0);
+        System.out.println(max_step);
+    }
 
-        System.out.println(ans);
+    public static void dfs(int y, int x) {
+        int alpha = arr[y][x];
+        visited[alpha] = true;
+
+        for(int i=0; i < 4; i++) {
+            int yy = dy[i] + y;
+            int xx = dx[i] + x;
+
+            if(yy < 0 || xx < 0 || yy >= ROW || xx >= COL) continue;
+
+            int nextAlpha = arr[yy][xx];
+            if(visited[nextAlpha]) continue;
+
+            max_step = Math.max(max_step, ++step);
+
+            dfs(yy, xx);
+        }
+        // 여기서 초기화 하지 않으면 다른 경로에서 접근 할 수 없다.
+        step --;
+        visited[alpha] = false;
     }
 }
+
