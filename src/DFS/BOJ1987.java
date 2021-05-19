ea279097c2c9c428af1a60d9eee2ed3ff94fe958
seungwon2 <dandelionsw2@ewhain.net>
2021-05-19 15:16:02 +0900

    :recycle: refactor : add comments to BOJ1987 code

--- src/DFS/BOJ1987.java
+++ src/DFS/BOJ1987.java
@@ -44,16 +44,22 @@ public class BOJ1987 {
         visited[alpha] = true;
 
         for(int i=0; i < 4; i++) {
+            //옮긴 좌표 xx, yy
             int yy = dy[i] + y;
             int xx = dx[i] + x;
 
+            //범위 밖이면 넘어감
             if(yy < 0 || xx < 0 || yy >= ROW || xx >= COL) continue;
 
+            //범위 안이면 해당 alpha를 뽑아옴
             int nextAlpha = arr[yy][xx];
+            //이미 방문했던거면 넘어감
             if(visited[nextAlpha]) continue;
 
+            //방문한거 아니면 step을 올려줌
             max_step = Math.max(max_step, ++step);
 
+            //dfs옮긴 좌표로 또 실시
             dfs(yy, xx);
         }
         // 여기서 초기화 하지 않으면 다른 경로에서 접근 할 수 없다.
