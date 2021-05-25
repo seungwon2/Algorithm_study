ba755b23581910ab651cb734470b984c32999113
seungwon2 <dandelionsw2@ewhain.net>
2021-05-25 10:27:18 +0900

    :recycle: refactor : add binary search logic to BOJ1300 code

--- src/BinarySearch/BOJ1300.java
+++ src/BinarySearch/BOJ1300.java
@@ -1,8 +1,5 @@
 package BinarySearch;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class BOJ1300 {
@@ -10,13 +7,26 @@ public class BOJ1300 {
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int K = s.nextInt();
-        List<Integer> B = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                B.add(i * j);
+        long left = 1, right = K;
+        long ans = 0;
+
+        // 이분 탐색 수행
+        while (left <= right) {
+            long mid = (left + right) / 2; // 임의의 수 지정
+            long cnt = 0;
+
+            // mid보다 작거나 같은 수는 몇 개인지 계산.
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(mid / i, N);
+            }
+
+            if (cnt < K) {
+                left = mid + 1;
+            } else {
+                ans = mid;
+                right = mid - 1;
             }
         }
-        Collections.sort(B);
-        System.out.println(B.indexOf(K-1));
+        System.out.println(ans);
     }
 }
